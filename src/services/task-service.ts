1cf129988698e95b4f96b3d6dbdb3d7345bd7034
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-10 22:22:10 +0500

    ADD: разработал сервис для работы с задачами

---
+++ src/services/task-service.ts
@@ -0,0 +1,17 @@
+import type { ITaskItem } from "@/types/task";
+
+class TaskService {
+  async getTasksList(): Promise<ITaskItem[]> {
+    const res = await fetch("/db/tasks.json", {
+      method: "get",
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+}
+
+export default new TaskService();
