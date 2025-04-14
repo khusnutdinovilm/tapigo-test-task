9efd12d9c13e125be52a30a0458de33e4c9231b9
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2025-04-14 14:54:41 +0500

    UPD: вывел шапку сайта и переписал стили

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <main>
+  <app-header />
+
+  <main class="container">
     <div class="task-items">
       <task-item v-for="task in taskStore.taskList" :key="task.id" :task-item="task" />
     </div>
@@ -8,6 +10,8 @@
 
 <script setup lang="ts">
 import { onMounted } from "vue";
+
+import AppHeader from "./components/app-header.vue";
 
 import TaskItem from "./components/task-item.vue";
 import useTaskStore from "./stores/task-store";
@@ -20,17 +24,11 @@ onMounted(async () => {
 </script>
 
 <style>
-main {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-
 .task-items {
   display: flex;
   flex-direction: column;
   gap: 10px;
-  border: 2px dashed black;
+  border: 1px solid var(--main-border-color);
   border-radius: 16px;
   padding: 16px;
 }
