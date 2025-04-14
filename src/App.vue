49d681feeba1e3c6102ab5c4c1cf9aac2ad9df59
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2025-04-14 13:35:30 +0500

    UPD: вывел список задач с новыми компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
   <main>
-    <div v-if="unDoneTasks.length !== 0" class="task-items">
-      <div
-        v-for="task in unDoneTasks"
-        class="task-item"
-        @click.prevent="taskStore.toggleTaskItem(task.id)"
-      >
-        <span>{{ task.title }}</span>
-      </div>
-    </div>
-
-    <div v-if="doneTasks.length !== 0" class="task-items">
-      <div
-        v-for="task in doneTasks"
-        class="task-item"
-        :class="{ 'task-item--checked': task.done }"
-        @click.prevent="taskStore.toggleTaskItem(task.id)"
-      >
-        <span>{{ task.title }}</span>
-      </div>
+    <div class="task-items">
+      <task-item v-for="task in taskStore.taskList" :key="task.id" :task-item="task" />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
+import { onMounted } from "vue";
 
+import TaskItem from "./components/task-item.vue";
 import useTaskStore from "./stores/task-store";
 
 const taskStore = useTaskStore();
-
-const doneTasks = computed(() => taskStore.taskList.filter(task => task.done));
-const unDoneTasks = computed(() => taskStore.taskList.filter(task => !task.done));
 
 onMounted(async () => {
   await taskStore.getTasksList();
@@ -52,15 +33,5 @@ main {
   border: 2px dashed black;
   border-radius: 16px;
   padding: 16px;
-}
-
-.task-item {
-  padding: 12px 16px;
-  border: 1px solid black;
-  border-radius: 12px;
-}
-
-.task-item--checked span {
-  text-decoration: line-through;
 }
 </style>
