ee31b455d51d4a6893044524a1992dcaae1d97fb
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2025-04-14 12:48:54 +0500

    UPD: обновил task-service, task-store

--- src/services/task-service.ts
+++ src/services/task-service.ts
@@ -1,16 +1,46 @@
 import type { ITaskItem } from "@/types/task";
 
 class TaskService {
-  async getTasksList(): Promise<ITaskItem[]> {
-    const res = await fetch("db/tasks.json", {
-      method: "get",
-    });
+  private tasks: ITaskItem[];
 
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
+  constructor() {
+    const lsTasks = localStorage.getItem("tasks");
+    this.tasks = lsTasks ? JSON.parse(lsTasks) : [];
+  }
+
+  private saveTasksInLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  }
+
+  async getTasksList(): Promise<ITaskItem[]> {
+    if (this.tasks) {
+      return this.tasks;
+    } else {
+      const res = await fetch("db/tasks.json", {
+        method: "get",
+      });
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+
+      this.tasks = await res.json();
+      this.saveTasksInLocalStorage();
+
+      return this.tasks;
+    }
+  }
+
+  async updateTaskItem(taskId: number): Promise<ITaskItem> {
+    const idx = this.tasks.findIndex(task => task.id === taskId);
+    if (idx === -1) {
+      throw new Error(`Задача с id=${taskId} не найдена`);
     }
 
-    return await res.json();
+    this.tasks[idx].done = !this.tasks[idx].done;
+    this.saveTasksInLocalStorage();
+
+    return this.tasks[idx];
   }
 }
 
--- src/stores/task-store.ts
+++ src/stores/task-store.ts
@@ -1,39 +1,35 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 import taskService from "@/services/task-service";
 import type { ITaskItem } from "@/types/task";
 
-const useTaskStore = defineStore("task-service", () => {
+const useTaskStore = defineStore("task-store", () => {
   const taskList = ref<ITaskItem[]>([]);
-
-  const doneTasks = computed(() => taskList.value.filter(task => task.done));
-  const unDoneTasks = computed(() => taskList.value.filter(task => !task.done));
 
   const getTasksList = async () => {
     taskList.value = await taskService.getTasksList();
-    saveTasksInLocalStorage();
   };
 
-  const saveTasksInLocalStorage = () =>
-    localStorage.setItem("tasks", JSON.stringify(taskList.value));
-
-  const toggleTaskItem = (taskId: number) => {
+  const toggleTaskItem = async (taskId: number) => {
     const idx = taskList.value.findIndex(task => task.id === taskId);
+    if (idx === -1) return;
 
-    if (idx === -1) {
-      throw new Error(`Элемента с id=${taskId} не существует`);
-    }
-
+    const oldTask = { ...taskList.value[idx] };
     taskList.value[idx].done = !taskList.value[idx].done;
-    saveTasksInLocalStorage();
+    taskList.value = [...taskList.value];
+
+    try {
+      await taskService.updateTaskItem(taskId);
+    } catch (error) {
+      taskList.value[idx] = oldTask;
+      taskList.value = [...taskList.value];
+    }
   };
 
   return {
-    doneTasks,
-    unDoneTasks,
+    taskList,
     getTasksList,
-    saveTasksInLocalStorage,
     toggleTaskItem,
   };
 });
